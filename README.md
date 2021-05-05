1c0b6d8f2483134ef3e040e2cb9c97b6de7e837b
yonle <fledyonle@gmail.com>
2021-05-06 01:04:11 +0800

    Nothing.

--- README.md
+++ README.md
@@ -21,13 +21,65 @@ obj.toHtml(process stdin);
 // <!doctype html><html><head><title>Hello world!</title></head><body><h1>Hello world!</h1></body></html>
 ```
 
-### Adding `src` on `img`
-```
+# API
+## `argsOnly`
+`argsOnly` is used for filling a argument into a tag like `meta` or `img`. It's used for Filling all required argument in `meta` or `img` tag.
+#### Example 1
+```json
 {
-	"img src='./image.png'": {
+	meta: {
+		argsOnly: true,
+		name: "description",
+		content: "My website!"
+	}
+}
+```
+Writes:
+```html
+<meta name="description" content="My website!">
+```
+#### Example 2
+```json
+{
+	img: {
+		argsOnly: true,
+		src: "..."
+	}
+}
+```
+Writes:
+```html
+<img src="...">
+```
+
+## `withCloseTag`
+`withCloseTag` is used for a tag like `script`, Same as `argsOnly` does but with close tag.
+
+```json
+{
+	script: {
+		withCloseTag: true,
+		src: "..."
+	}
+}
+```
+Writes:
+```html
+<script src="..."></script>
+```
+
+## Alternative way to Writting argument
+We aren't only giving a trick like that, We're also doing `HTML Argument Mode` on this package!
+```json
+{
+	"div id='box'": {
 		....
 	}
 }
+```
+Writes: 
+```html
+<div id='box'> ..... </div>
 ```
 # Community
 - [Discord](https://dsc.gg/yonle)
--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+const filter = ["toHtml", "argsOnly", "withCloseTag"];
+
 Object.prototype.toHtml = function toHtml ( c, noDoctype ) {
  	let obj = this;
 	if (!noDoctype) c.write("<!doctype html>");
 	for ( let i in obj ) {
-		if (i == "toHtml") continue;
+		if (filter.includes(i.split(" ")[0])) continue;
 	    if (typeof obj[i] == "object") {
-	        c.write(`<${i}>`);
-			obj[i].toHtml(c, true);
-			c.write(`</${i.split(" ")[0]}>`);
-	    	continue;
+	    	if (obj[i].argsOnly) {
+	    		c.write(`<${i}`);
+	    		for ( let name in obj[i] ) {
+	    			if (filter.includes(name.split(" ")[0])) continue;
+	    			c.write(` ${name}="${obj[i][name]}"`)
+	    		}
+	    		c.write(">");
+	    		continue;
+	    	} else {
+	    		if (obj[i].withCloseTag) {
+	    			c.write(`<${i}`);
+	    			for ( let name in obj[i] ) {
+	    				if (filter.includes(name.split(" ")[0])) continue;
+	    				c.write(` ${name}="${obj[i][name]}"`)
+	    			}
+	    			c.write(`></${i}>`);
+	    			continue
+	    		} else {
+	        		c.write(`<${i}>`);
+	        	}
+				obj[i].toHtml(c, true);
+				c.write(`</${i.split(" ")[0]}>`);
+	    		continue;
+	    	}
 	    }
 		c.write(`<${i}>${obj[i]}</${i.split(" ")[0]}>`);
 	}
-	() => c.end();
 }
